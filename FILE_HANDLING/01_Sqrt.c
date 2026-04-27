26bbf87e514fb8280cf1575c896d71fd1d3e7c26
siddh-jain01 <siddhjain430@gmail.com>
2026-04-27 14:49:54 +0530

    adding new code

---
+++ FILE_HANDLING/01_Sqrt.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float x = sqrt(3);
+    printf("square is %f ",x);
+}
---
+++ FILE_HANDLING/02_Cbrt.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float x = cbrt(3);
+    printf("cube is %f ",x);
+}
