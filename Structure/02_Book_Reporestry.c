775f51fda09d60c1683680f6bc4d839c7d867897
siddh-jain01 <siddhjain430@gmail.com>
2026-05-04 23:36:18 +0530

    adding new code

--- Structure/02_Book_Reporestry.c
+++ Structure/02_Book_Reporestry.c
@@ -10,34 +10,34 @@ struct Book {
 
 int main() {
   
-//     struct Book b;
+    struct Book b;
 
 
-//     printf("Enter book id\n ");
-//     scanf("%d",&b.id);
-//     printf("Enter name\n ");
-//     scanf("%c",&b.name);
-//     printf("Enter author\n");
-//     scanf("%c",&b.author);
-//     printf("Enter price\n");
-//     scanf("%f",&b.price);
-struct Book b[5];
-
- int i ,n ;
-
-       printf("enter the number book ");
-       scanf("%d,&n");
-
-       for (i=0;i<n;i++){
     printf("Enter book id\n ");
-    scanf("%d",&b[i].id);
+    scanf("%d",&b.id);
     printf("Enter name\n ");
-    scanf("%c",&b[i].name);
+    scanf("%c",&b.name);
     printf("Enter author\n");
-    scanf("%c",&b[i].author);
+    scanf("%c",&b.author);
     printf("Enter price\n");
-    scanf("%f",&b[i].price);
-        }
+    scanf("%f",&b.price);
+// struct Book b[5];
+
+//  int i ,n ;
+
+//        printf("enter the number book ");
+//        scanf("%d,&n");
+
+//        for (i=0;i<n;i++){
+//     printf("Enter book id\n ");
+//     scanf("%d",&b[i].id);
+//     printf("Enter name\n ");
+//     scanf("%c",&b[i].name);
+//     printf("Enter author\n");
+//     scanf("%c",&b[i].author);
+//     printf("Enter price\n");
+//     scanf("%f",&b[i].price);
+        // }
 
 
 
